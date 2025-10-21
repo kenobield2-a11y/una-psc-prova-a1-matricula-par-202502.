10f00d8c0f847124ebb6ec48f73acbe430a814c7
GABRIEL MOREIRA <kenobield2@gmail.com>
2025-10-21 20:57:35 -0300

    Add files via upload

---
+++ ArattaiGrowthTracker.java
@@ -0,0 +1,31 @@
+//Gabriel Moreira da Silva
+325128236******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.ArrayList;
+public class Main
+{
+	public static void main(String[] args) {
+	    int dowloadstotais;pico
+	    ArrayList<String> diasDaSemana = new ArrayList<String>();
+	    
+	    diasDaSemana.add("dom");
+	    diasDaSemana.add("seg");
+	    diasDaSemana.add("ter");
+	    diasDaSemana.add("qua");
+	    diasDaSemana.add("qui");
+	    diasDaSemana.add("sex");
+	    diasDaSemana.add("sab");
+	   
+	    dowloadstotais=850000+ 1050000+ 1100000+ 950000+ 1200000+ 1000000+ 850000;
+	    pico=1200000
+	    menor=850000
+	    System.out.println("Total de Downloads na Semana:"+dowloadstotais);
+	     
+	    
+	    }
+}
